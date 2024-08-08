f30c034da047414b4958d84397263210b82f2e4f
Milad-Akbari <milad.akbari.st@gmail.com>
2024-08-08 16:37:50 +0330

    TableRow Extarct

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,39 +1,60 @@
-import chartUp from "../../assets/chart-up.svg"
-import chartDown from "../../assets/chart-down.svg"
-function TableCoins({coins}) {
-    console.log(coins);
-  return <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Coin</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>24h</th>
-          <th>Total Volume</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {coins.map(coin => <tr key={coin.id}>
-          <td>
-            <div>
-              <img src={coin.image} alt={coin.name} />
-              <span>{coin.symbol.toUpperCase()}</span>
-            </div>
-          </td>
-          <td>{coin.name}</td>
-          <td>$ {coin.current_price.toLocaleString()}</td>
-          <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-          <td>{coin.total_volume.toLocaleString()}</td>
-          <td>
-            <img src={coin.price_change_percentage_24h>0 ? chartUp : chartDown} alt={coin.name} />
-          </td>
-        </tr>)}
-      </tbody>
-    </table>
-  </div>;
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
+function TableCoins({ coins }) {
+  console.log(coins);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <TableRow coin={coin} key={coin.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TableCoins;
 
+const TableRow = ({
+  coin: {
+    name,
+    image,
+    symbol,
+    current_price,
+    total_volume,
+    price_change_percentage_24h :price_change,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} alt={name} />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>$ {current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img
+          src={price_change > 0 ? chartUp : chartDown}
+          alt={name}
+        />
+      </td>
+    </tr>
+  );
+};
