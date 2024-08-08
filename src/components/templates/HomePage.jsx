895c930d5bcabd25a5dd6bb008f68720da55f596
Milad-Akbari <milad.akbari.st@gmail.com>
2024-08-08 14:02:13 +0330

    make cryptoApi for recive Data

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,17 +1,19 @@
 import { useEffect } from "react";
 import TableCoins from "../modules/TableCoins";
 import { useState } from "react";
+import {getCoinList} from "../../services/cryptoApi"
 
 function HomePage() {
   const [coins , setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
-    )
-      .then((res) => res.json())
-      .then((res) => setCoins(res));
+   const getData = async() =>{
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json)
+   }
+   getData()
   }, []);
-  
+
   return <div>
     <TableCoins coins={coins}/>
   </div>;
