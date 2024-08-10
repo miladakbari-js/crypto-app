890496936627a47055252628fd3e3143e5ff5a38
Milad-Akbari <milad.akbari.st@gmail.com>
2024-08-10 18:01:29 +0330

    handle for search 01

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -9,23 +9,27 @@ function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [currency , setCurrency] = useState("usd")
+  const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page , currency));
-      const json = await res.json();
-      setCoins(json);
-      setIsLoading(false);
+      try {
+        const res = await fetch(getCoinList(page, currency));
+        const json = await res.json();
+        setCoins(json);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
     getData();
-  }, [page , currency]);
+  }, [page, currency]);
 
   return (
     <div>
-      <Search currency={currency} setCurrency={setCurrency}/>
-      <TableCoins coins={coins} isLoading={isLoading} currency={currency}/>
+      <Search currency={currency} setCurrency={setCurrency} />
+      <TableCoins coins={coins} isLoading={isLoading} currency={currency} />
       <Pagination page={page} setPage={setPage} />
     </div>
   );
