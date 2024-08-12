e7072271f3961b803e60861d46ed17ebb3b540c8
Milad-Akbari <milad.akbari.st@gmail.com>
2024-08-12 23:15:48 +0330

    graph style

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,14 +1,14 @@
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 import { RotatingLines } from "react-loader-spinner";
-import styles from "./TableCoins.module.css"
+import styles from "./TableCoins.module.css";
 import { marketChart } from "../../services/cryptoApi";
 
-function TableCoins({ coins, isLoading , currency , setChart}) {
+function TableCoins({ coins, isLoading, currency, setChart }) {
   return (
     <div className={styles.container}>
       {isLoading ? (
-        <RotatingLines strokeColor="#3874ff" strokeWidth="2"/>
+        <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
       ) : (
         <table className={styles.table}>
           <thead>
@@ -23,7 +23,12 @@ function TableCoins({ coins, isLoading , currency , setChart}) {
           </thead>
           <tbody>
             {coins.map((coin) => (
-              <TableRow coin={coin} key={coin.id} currency={currency} setChart={setChart}/>
+              <TableRow
+                coin={coin}
+                key={coin.id}
+                currency={currency}
+                setChart={setChart}
+              />
             ))}
           </tbody>
         </table>
@@ -34,8 +39,8 @@ function TableCoins({ coins, isLoading , currency , setChart}) {
 
 export default TableCoins;
 
-const TableRow = ({
-  coin: {
+const TableRow = ({ coin, currency, setChart }) => {
+  const {
     id,
     name,
     image,
@@ -43,27 +48,26 @@ const TableRow = ({
     current_price,
     total_volume,
     price_change_percentage_24h: price_change,
-  },currency , setChart
-}) => {
-  const currencyHelper = (currency)=>{
-      if(currency === "usd"){
-        return <span>$</span>
-      }else if(currency === "eur"){
-        return <span>€</span>
-      }else{
-        return <span>¥</span>
-      }
-  }
-  const showHandler = async ()=>{
+  } = coin;
+
+  const currencyHelper = (currency) => {
+    if (currency === "usd") {
+      return <span>$</span>;
+    } else if (currency === "eur") {
+      return <span>€</span>;
+    } else {
+      return <span>¥</span>;
+    }
+  };
+  const showHandler = async () => {
     try {
       const res = await fetch(marketChart(id));
       const json = await res.json();
-      setChart(json);
-      console.log(json);
+      setChart({...json , coin});
     } catch (error) {
-      setChart(null)
+      setChart(null);
     }
-  }
+  };
   return (
     <tr>
       <td>
@@ -73,8 +77,12 @@ const TableRow = ({
         </div>
       </td>
       <td>{name}</td>
-      <td>{currencyHelper(currency)} {current_price.toLocaleString()}</td>
-      <td className={price_change>0 ? styles.success : styles.error}>{price_change.toFixed(2)}%</td>
+      <td>
+        {currencyHelper(currency)} {current_price.toLocaleString()}
+      </td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
